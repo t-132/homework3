650432a058068038cea83abccf448747599f513e
t-132 <svp4419@gmail.com>
2022-12-06 17:36:41 +0300

    1

---
+++ HomeWork3/Program.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using System;
+using System.IO;
+using Serilog.Sinks.SpectreConsole;
+
+namespace HomeWork3
+{
+   class Program
+   {
+      public static void Main(string[] args)
+      {
+         var builder = new ConfigurationBuilder();
+         BuildConfig(builder);
+
+         Log.Logger = new LoggerConfiguration()
+            .ReadFrom.Configuration(builder.Build())
+            .Enrich.FromLogContext()
+            .WriteTo.SpectreConsole()
+            .CreateLogger();
+
+         Log.Logger.Information("Starting...");
+         
+         var host = Host.CreateDefaultBuilder()            
+            .ConfigureServices((ctx, srv) =>
+                {
+                   srv.AddTransient<IAppService, AppService>();
+                   srv.
+                })
+            .UseSerilog()
+            .Build();
+         var svc = ActivatorUtilities.CreateInstance<AppService>(host.Services);
+         svc.Run();
+      }
+
+      public static void BuildConfig(IConfigurationBuilder builder)
+      {
+         builder.SetBasePath(Directory.GetCurrentDirectory())
+           .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+           .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ENVIRONMENT") ?? "Production"}.json", optional: true)
+           .AddEnvironmentVariables();
+      }
+   }
+}
---
+++ HomeWork3/appService.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace HomeWork3
+{
+   public class AppService : IAppService
+   {
+      private readonly ILogger<AppService> _log;
+      private readonly IConfiguration _config;
+
+      public AppService(ILogger<AppService> log, IConfiguration config)
+      {
+         _log = log;
+         _config = config;
+      }
+
+      public void Run()
+      {
+         for (int i = 0; i <= _config.GetValue<int>("count"); i++)
+         {
+            _log.LogInformation("Im live {count}", i);
+         }
+      }
+   }
+}
